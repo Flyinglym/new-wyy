6f11c00354bfc7aa19bd3e25f82d0dc7e44a519e
1953582865@qq.com <1953582865@qq.com>
2023-03-16 16:44:09 +0800

    test(deps): 初始化项目

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,41 +1,51 @@
 module.exports = {
-	"env": {
-		"browser": true,
-		"es2021": true,
-		"node": true
-	},
-	extends: [
-		'plugin:vue/essential',
-		'airbnb-base',
-		'plugin:prettier/recommended' // 添加 prettier 插件
-	],
-	"overrides": [
-	],
-	"parser": "@typescript-eslint/parser",
-	"parserOptions": {
-		"ecmaVersion": "latest",
-		"sourceType": "module"
-	},
-	"plugins": [
-		"vue",
-		"@typescript-eslint"
-	],
-	"rules": {
-		"indent": [
-			"error",
-			"tab"
-		],
-		"linebreak-style": [
-			"error",
-			"unix"
-		],
-		"quotes": [
-			"error",
-			"double"
-		],
-		"semi": [
-			"error",
-			"always"
-		]
-	}
-};
+  'env': {
+    'browser': true,
+    'es2021': true,
+    'node': true
+  },
+  extends: [
+    'plugin:vue/essential',
+    'airbnb-base',
+    'plugin:prettier/recommended', // 添加 prettier 插件
+    'plugin:jest/recommended'
+  ],
+  'overrides': [
+    {
+      files: [
+        '**/__tests__/*.{j,t}s?(x)',
+        '**/tests/unit/**/*.spec.{j,t}s?(x)'
+      ],
+      env: {
+        jest: true
+      }
+    }
+  ],
+  'parser': '@typescript-eslint/parser',
+  'parserOptions': {
+    'ecmaVersion': 'latest',
+    'sourceType': 'module'
+  },
+  'plugins': [
+    'vue',
+    '@typescript-eslint'
+  ],
+  'rules': {
+    'indent': [
+      'error',
+      'tab'
+    ],
+    'linebreak-style': [
+      'error',
+      'unix'
+    ],
+    'quotes': [
+      'error',
+      'double'
+    ],
+    'semi': [
+      'error',
+      'always'
+    ]
+  }
+}
